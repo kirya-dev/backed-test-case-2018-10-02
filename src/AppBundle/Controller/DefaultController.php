e720d6f5092cd46c0fcf8fa1d3af1d9e65aada38
Kirill Matasov <kirill.matasov@immelman.ru>
2018-10-02 12:43:09 +0300

    Optimized

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\Department;
 use AppBundle\Entity\Stuff;
+use Doctrine\ORM\QueryBuilder;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,31 +15,36 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        $stuffMap = [];
+        /** @var QueryBuilder $qb */
+        $qb = $this->getDoctrine()->getManager()->createQueryBuilder('d');
 
-        /** @var Stuff[] $stuffs */
-        $stuffs = $this->getDoctrine()->getRepository(Stuff::class)->findAll();
-        foreach ($stuffs as $stuff) {
-            $stuffMap[$stuff->getId()] = $stuff;
-        }
-
+        $q = $qb->select('d.id as dId, d.name as dName, s.id as sId, s.firstName as sFName, s.lastName as sLName')
+            ->from(Department::class, 'd')
+            ->leftJoin('d.stuff', 's')
+            ->getQuery()
+        ;
 
         $table = [];
+        $stuffMap = [];
 
-        /** @var Department[] $departments */
-        $departments = $this->getDoctrine()->getRepository(Department::class)->findAll();
+        foreach ($q->getArrayResult() as $res) {
+            $dId = $res['dId'];
+            $sId = $res['sId'];
 
-        foreach ($departments as $department) {
-            $stuffIds = [];
-            foreach ($department->getStuff() as $stuff) {
-                $stuffIds[] = $stuff->getId();
+            if (!isset($table[$dId])) {
+                $table[$dId] = [
+                    'department' => ['name' => $res['dName']],
+                    'stuffIds'    => []
+                ];
             }
 
-            $table[] = [
-                'department' => $department,
-                'stuffIds' => $stuffIds,
-            ];
+            if (!isset($stuffMap[$sId])) {
+                $stuffMap[$sId] = ['lastName' => $res['sLName'], 'firstName' => $res['sFName']];
+            }
+
+            $table[$dId]['stuffIds'][] = $sId;
         }
+
 
         return $this->render('default/index.html.twig', [
             'stuffMap' => $stuffMap,
